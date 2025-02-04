162738dfb23fbb29c975345eeede0f405ccb37ad
Renan Lobo <84355075+c4n15lupu5jr@users.noreply.github.com>
2025-02-04 16:05:35 -0300

    blog-next

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,18 @@
 import React from 'react';
 import { PostData } from '../domain/posts/post';
 import { GetStaticProps } from 'next';
-
-const getPosts = async (): Promise<PostData[]> => {
-    const posts = await fetch(
-        'https://evening-retreat-45125.herokuapp.com/posts',
-    );
-    const jsonPosts = await posts.json();
-    return jsonPosts;
-};
+import { getAllPosts } from '../data/posts/get-all-posts';
 
 export type HomeProps = {
     posts: PostData[];
-};
-
-export default function Home() {
-    return (
-        <div>
-            {posts.map((post) => (
-                <h2 key={post.slug}>{post.title}</h2>
-            ))}
-        </div>
-    );
 }
 
-export const getStaticProps: GetStaticProps = async (context) => {
-    const posts = await getPosts();
+export default function Home({ posts }: HomeProps) {
+    return <HomePage posts={posts} />
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+    const posts = await getAllPosts();
 
     return {
         props: { posts },
